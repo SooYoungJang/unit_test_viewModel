9d0da26866f776fabca1ad462cc1d0d86f6d4fb7
SooYoungJang <tturrr10@gmail.com>
2021-07-29 21:36:37 +0900

    test 코드 설명 주석 추가

--- app/src/test/java/com/anushka/circlecalc/CalcViewModelTest.kt
+++ app/src/test/java/com/anushka/circlecalc/CalcViewModelTest.kt
@@ -13,11 +13,11 @@ class CalcViewModelTest{
     private lateinit var calcViewModel: CalcViewModel
     private lateinit var calculations: Calculations
 
-    @get:Rule
-    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    @get:Rule //test 전 / 후에 정의된 코드들이 실행되도록 만든다.
+    var instantTaskExecutorRule = InstantTaskExecutorRule() // liveData의 synchronous 하게 테스팅 가능하도록 하는 코드.
 
     @Before
-    fun setUp() {
+    fun setUp() { //testing 전에 필요한 데이터를 세팅해두는 곳. 예를들어 Mockito를 사용해서 Mock데이터를 만들어 둘 수 있다.
         calculations = Mockito.mock(Calculations::class.java)
         Mockito.`when`(calculations.calculateArea(2.1)).thenReturn(13.8474)
         Mockito.`when`(calculations.calculateCircumference(1.0)).thenReturn(6.28)
